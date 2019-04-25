efafdc55000adfb1970a141a59ff9ba658a965e0
medrimonia <medrimonia@gmail.com>
2019-04-25 23:47:55 +0200

    Taking into account player extra information

--- src/qt_monitoring/player_widget.cpp
+++ src/qt_monitoring/player_widget.cpp
@@ -1,5 +1,10 @@
 #include "player_widget.h"
 
+#include <hl_communication/utils.h>
+#include <rhoban_team_play/extra_team_play.pb.h>
+
+using namespace rhoban_team_play;
+
 namespace qt_monitoring
 {
 PlayerWidget::PlayerWidget() : player_id(0), robot_name("robot")
@@ -27,7 +32,18 @@ void PlayerWidget::treatMessage(const hl_communication::RobotMsg& robot_msg)
 {
   std::string robot_text = "Player " + std::to_string(robot_msg.robot_id().robot_id());
   robot_label->setText(robot_text.c_str());
+  if (!robot_msg.has_free_field())
+  {
+    throw std::logic_error(HL_DEBUG + "no free field (Rhoban standard)");
+  }
+  MiscExtra extra;
+  extra.ParseFromString(robot_msg.free_field());
   // TODO:
+  referee_label->setText(("referee: " + extra.referee()).c_str());
+  robocup_label->setText(("robocup: " + extra.robocup()).c_str());
+  playing_label->setText(("playing: " + extra.playing()).c_str());
+  search_label->setText(("search: " + extra.search()).c_str());
+  hardware_label->setText(("hardware: " + extra.hardware_warnings()).c_str());
 }
 
 }  // namespace qt_monitoring
